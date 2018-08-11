0dc7f9afef657c867b50621aeff3f0a300f0803f
7erkin <olegsemerkin777@gmail.com>
2018-08-11 17:38:53 +0300

    Исправил замечания

--- js/main.js
+++ js/main.js
@@ -1,113 +1,134 @@
 'use strict';
 
-(function () {
-  let currentScreenId;
-  const rootNode = document.querySelector(`#main`);
-  const KeyCode = {
-    LEFT_ARROW: 37,
-    RIGHT_ARROW: 39
-  };
-  let screensNodes;
+const rootNode = document.querySelector(`#main`);
+const KeyName = {
+  LEFT_ARROW: `ArrowLeft`,
+  RIGHT_ARROW: `ArrowRight`
+};
+const orderScreenById = [
+  `greeting`,
+  `intro`,
+  `rules`,
+  `game-1`,
+  `game-2`,
+  `game-3`,
+  `stats`,
+  `modal-error`,
+  `modal-confirm`
+];
+let buttonsArrowsNodes;
+let currentScreenId;
+let screensNodes;
 
-  /** Получает узлы шаблонов всех экранов для отрисовки
-   * @return {Array}
-   */
-  let getScreensNodes = function () {
-    let templatesNodes = document.querySelectorAll(`template[data-type="screen"]`);
-    return Array.prototype.map.call(templatesNodes, (templateNode) => {
-      return templateNode.content.querySelector(`div`).cloneNode(true);
-    }).sort((node1, node2) => {
-      return node1.dataset.id - node2.dataset.id;
-    });
-  };
+let embedNode = function (nodeWhere, nodeWhat) {
+  nodeWhere.appendChild(nodeWhat);
+};
 
-  /** Проверяет, возможно ли следующее переключение на другой экран
-   * @param {Number} code Код нажатой при переключении кнопки
-   * @return {Boolean}
-   */
-  let isNextScreenPossible = function (code) {
-    return code === KeyCode.LEFT_ARROW ? currentScreenId - 1 >= 0 : currentScreenId + 1 <= screensNodes.length - 1;
-  };
+/** Получает узлы шаблонов всех экранов для отрисовки
+  * @return {Array}
+  */
+let getScreensNodes = function () {
+  let nodes = [];
+  orderScreenById.forEach((id) => {
+    let templateNode = document.querySelector(`#${id}`).content.querySelector(`div`);
+    nodes.push(templateNode.cloneNode(true));
+  });
+  return nodes;
+};
 
-  /** Устанавливает очередной экран
-   * @param {Number} code Код нажатой при переключении кнопки
-   */
-  let setNextScreen = function (code) {
-    if (code === KeyCode.LEFT_ARROW) {
-      setScreenById(currentScreenId - 1);
-      return;
-    }
-    setScreenById(currentScreenId + 1);
-  };
-  let getArrowButtonKey = function (evt) {
-    return evt.target.dataset.buttonType === `left` ? KeyCode.LEFT_ARROW : KeyCode.RIGHT_ARROW;
-  };
+let getButtonsArrows = function () {
+  return document.querySelectorAll(`.arrows__btn`);
+};
 
-  /** Обработчик события на переключение экрана
-   * @param {Event} evt
-   */
-  let onScreenSwitched = function (evt) {
-    if (evt.keyCode !== undefined && !Object.keys(KeyCode).some((code) => {
-      return KeyCode[code] === evt.keyCode;
-    })) {
-      return;
-    }
-    if (evt.keyCode === undefined && evt.target.dataset.buttonType !== undefined) {
-      evt.keyCode = getArrowButtonKey(evt);
-    } else {
-      return;
-    }
-    if (!isNextScreenPossible(evt.keyCode)) {
-      return;
-    }
-    setNextScreen(evt.keyCode);
-  };
+/** Проверяет, возможно ли следующее переключение на другой экран
+  * @param {Number} code Код нажатой при переключении кнопки
+  * @return {Boolean}
+  */
+let isNextScreenPossible = function (code) {
+  return code === KeyName.LEFT_ARROW ? currentScreenId - 1 >= 0 : currentScreenId + 1 <= screensNodes.length - 1;
+};
 
-  /** Удаляет текущий экран
-   */
-  let removeScreen = function () {
-    Array.prototype.forEach.call(rootNode.children, (node) => {
-      node.remove();
-    });
-  };
+let changeScreen = function (keyName) {
+  if (!isNextScreenPossible(keyName)) {
+    return;
+  }
+  if (keyName === KeyName.LEFT_ARROW) {
+    setScreenById(currentScreenId - 1);
+    return;
+  }
+  setScreenById(currentScreenId + 1);
+};
 
-  /** Отрисовывает экран
-   * @param {Number} id Индекс экрана в массиве
-   */
-  let renderScreen = function (id) {
-    rootNode.appendChild(screensNodes[id]);
-  };
+let getButtonArrowKeyName = function (evt) {
+  let node = evt.target;
+  if (node.localName !== `button`) {
+    return undefined;
+  }
+  return node === buttonsArrowsNodes[0] ? KeyName.LEFT_ARROW : KeyName.RIGHT_ARROW;
+};
 
-  /** Обновляет экран
-   * @param {Number} id Индекс очередного экрана для отображения
-   */
-  let updateScreenById = function (id) {
-    removeScreen();
-    renderScreen(id);
-  };
+let getButtonKeyName = function (evt) {
+  let match = Object.keys(KeyName).some((name) => {
+    return KeyName[name] === evt.key;
+  });
+  return match ? evt.key : undefined;
+};
 
-  let setScreenById = function (id) {
-    currentScreenId = id;
-    updateScreenById(id);
-  };
+let getKeyName = function (evt) {
+  return evt.key === undefined ? getButtonArrowKeyName(evt) : getButtonKeyName(evt);
+};
 
-  /** Отрисовывает стрелки переключения на экран
-   */
-  let renderArrow = function () {
-    let templateArrowsNode = document.querySelector(`#arrows`).content.querySelector(`div`);
-    document.body.appendChild(templateArrowsNode.cloneNode(true));
-    document.querySelector(`.arrows__wrap`).addEventListener(`click`, onScreenSwitched);
-  };
+let onChangeScreen = function (evt) {
+  let keyName = getKeyName(evt);
+  if (keyName === undefined) {
+    return;
+  }
+  changeScreen(keyName);
+};
 
-  /** Устанавливает страницу в начальное состояние
-   */
-  let initializePage = function () {
-    screensNodes = getScreensNodes();
-    setScreenById(0);
-    renderArrow();
-    document.addEventListener(`keydown`, onScreenSwitched);
-  };
+/** Удаляет текущий экран
+  */
+let removeScreen = function () {
+  rootNode.innerHTML = ``;
+};
 
-  initializePage();
-})();
+/** Отрисовывает экран
+  * @param {Number} id Индекс экрана в массиве
+  */
+let renderScreen = function (id) {
+  embedNode(rootNode, screensNodes[id]);
+};
+
+/** Обновляет экран
+  * @param {Number} id Индекс очередного экрана для отображения
+  */
+let updateScreenById = function (id) {
+  removeScreen();
+  renderScreen(id);
+};
+
+let setScreenById = function (id) {
+  currentScreenId = id;
+  updateScreenById(id);
+};
+
+/** Отрисовывает стрелки переключения на экран
+  */
+let renderArrow = function () {
+  let templateButtonsArrowsNode = document.querySelector(`#arrows`).content.querySelector(`div`);
+  document.body.appendChild(templateButtonsArrowsNode.cloneNode(true));
+};
+
+/** Устанавливает страницу в начальное состояние
+  */
+let initializePage = function () {
+  screensNodes = getScreensNodes();
+  setScreenById(0);
+  renderArrow();
+  buttonsArrowsNodes = getButtonsArrows();
+  document.querySelector(`.arrows__wrap`).addEventListener(`click`, onChangeScreen);
+  document.addEventListener(`keydown`, onChangeScreen);
+};
+
+initializePage();
 
